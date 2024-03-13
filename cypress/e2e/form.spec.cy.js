fd12b9fc4b1d47aea48c53286c3e4cfeaf22b9d3
lucas-hub <54378079+lucas-hub@users.noreply.github.com>
2024-03-13 16:08:27 -0300

    Adding last tests

--- cypress/e2e/form.spec.cy.js
+++ cypress/e2e/form.spec.cy.js
@@ -3,6 +3,7 @@
 describe('Form Testing', () => {
   
   const longText = Cypress._.repeat('Teste Teste Teste Teste Teste ', 10)
+  const breakingText = Cypress._.repeat('Teste Teste Teste Teste Teste ', 100)
   const red = 'rgb(245, 46, 46)'
   const green = 'rgb(0, 194, 43)'
 
@@ -34,7 +35,7 @@ describe('Form Testing', () => {
     cy.get('#nome-completo').type('Lucas Marco').should('have.value', 'Lucas Marco')
     cy.get('#email').type('lucas@.com').should('have.value', 'lucas@.com')
     cy.get('#telefone').type(' ').should('have.value', ' ')
-    cy.get('#mensagem').type(longText, {delay: 0}).should('have.value', longText)
+    cy.get('#mensagem').type(longText, {delay : 0}).should('have.value', longText)
     
     cy.get('#submit').click()
     
@@ -44,5 +45,18 @@ describe('Form Testing', () => {
     cy.get('#mensagem').should('have.css', 'border-color', green) 
 
     // The test fails since the telefone accepts non-numerical characters and all other accept blank spaces. See 'Report-form-2.txt' for more details.
+  })
+
+  it('Not writing any information on the fields and sending it', () => {   
+    cy.get('#submit').click()
+    
+    cy.get('#nome-completo').should('have.css', 'border-color', red)
+    cy.get('#email').should('have.css', 'border-color', red)
+    cy.get('#telefone').should('have.css', 'border-color', red) 
+    cy.get('#mensagem').should('have.css', 'border-color', red)
+  })
+
+  it.only('Testing the capacity of the message form', ()=> {
+    cy.get('#mensagem').type(breakingText, {delay : 0}).should('have.value', breakingText)
   })
 })
