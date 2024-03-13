eaa9e234023a349c16cad23918e89264478add72
lucas-hub <54378079+lucas-hub@users.noreply.github.com>
2024-03-13 15:46:05 -0300

    New bug report file and testing filling the form incorrectly

--- cypress/e2e/form.spec.cy.js
+++ cypress/e2e/form.spec.cy.js
@@ -3,6 +3,8 @@
 describe('Form Testing', () => {
   
   const longText = Cypress._.repeat('Teste Teste Teste Teste Teste ', 10)
+  const red = 'rgb(245, 46, 46)'
+  const green = 'rgb(0, 194, 43)'
 
   beforeEach(()=>{
     cy.visit('./index.html')
@@ -20,11 +22,27 @@ describe('Form Testing', () => {
     
     cy.get('#submit').click()
     
-    cy.get('#nome-completo').should('have.css', 'border-color', 'rgb(0, 194, 43)')
-    cy.get('#email').should('have.css', 'border-color', 'rgb(0, 194, 43)')
-    cy.get('#telefone').should('have.css', 'border-color', 'rgb(0, 194, 43)')
-    cy.get('#mensagem').should('have.css', 'border-color', 'rgb(0, 194, 43)')
+    cy.get('#nome-completo').should('have.css', 'border-color', green)
+    cy.get('#email').should('have.css', 'border-color', green)
+    cy.get('#telefone').should('have.css', 'border-color', green)
+    cy.get('#mensagem').should('have.css', 'border-color', green)
 
     // The tests are passing but there is no user feedback that the form was sent. See 'Report-form.txt' for more details.
+  })
+
+  it('Fill the form incorrectly and sends it', () => {
+    cy.get('#nome-completo').type('Lucas Marco').should('have.value', 'Lucas Marco')
+    cy.get('#email').type('lucas@.com').should('have.value', 'lucas@.com')
+    cy.get('#telefone').type(' ').should('have.value', ' ')
+    cy.get('#mensagem').type(longText, {delay: 0}).should('have.value', longText)
+    
+    cy.get('#submit').click()
+    
+    cy.get('#nome-completo').should('have.css', 'border-color', green)
+    cy.get('#email').should('have.css', 'border-color', green)
+    cy.get('#telefone').should('have.css', 'border-color', red) 
+    cy.get('#mensagem').should('have.css', 'border-color', green) 
+
+    // The test fails since the telefone accepts non-numerical characters and all other accept blank spaces. See 'Report-form-2.txt' for more details.
   })
 })
