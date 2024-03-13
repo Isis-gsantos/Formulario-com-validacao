8bf4924fa6c563e26b3535015347612209f6de77
lucas-hub <54378079+lucas-hub@users.noreply.github.com>
2024-03-13 15:24:43 -0300

    More tests and bug report

--- cypress/e2e/form.spec.cy.js
+++ cypress/e2e/form.spec.cy.js
@@ -17,7 +17,14 @@ describe('Form Testing', () => {
     cy.get('#email').type('lucas@gmail.com').should('have.value', 'lucas@gmail.com')
     cy.get('#telefone').type('999999999').should('have.value', '999999999')
     cy.get('#mensagem').type(longText, {delay: 0}).should('have.value', longText)
+    
     cy.get('#submit').click()
-    cy.get('#nome-completo').should('have.css', 'background-color', 'rgb(60, 204, 135)')
+    
+    cy.get('#nome-completo').should('have.css', 'border-color', 'rgb(0, 194, 43)')
+    cy.get('#email').should('have.css', 'border-color', 'rgb(0, 194, 43)')
+    cy.get('#telefone').should('have.css', 'border-color', 'rgb(0, 194, 43)')
+    cy.get('#mensagem').should('have.css', 'border-color', 'rgb(0, 194, 43)')
+
+    // The tests are passing but there is no user feedback that the form was sent. See 'Report-form.txt' for more details.
   })
 })
